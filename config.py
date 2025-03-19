5f25ab9a8864b7031a7914e32617b511d008d70c
Sachinrao786 <sr8749439@gmail.com>
2025-03-20 00:02:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,104 @@
+#Recoded by @Its_Oreki_Hotarou
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7526754954:AAH5eDysZFKvuNfqvgunG4p8sjfbeAot1b4")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "21672202"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "2e1f330564f445d74f4f9d2cd5782c0a")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002187340655"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6179290520"))
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
+DB_NAME = os.environ.get("DATABASE_NAME", "Its_Bumblebee_bot")
+
+#force sub channel id, if you want enable force sub
+FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002232449924"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001919440441"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#pics
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/rGy.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/rGX.jpg")
+
+#text
+HELP_TXT = "<blockquote><b>Hi Dude!\n\nTo use this bot you just have to join both channels that's it..\nWatch Tutorial to open Link - <a href=https://t.me/+ZLu08PF-JUIzMjFl>Clickhere</a></b></blockquote>"
+ABOUT_TXT = "<blockquote><b><i>About Us..\n\n‣ Made for : <a href=https://t.me/+sOF6oYv8MPVmOGQ1>ClickHere</a>\n‣ Owned by : @Mr_Goku_UI\n‣ Maintained by : @Mr_Goku_UI\n‣ Developed by : @Mr_Goku_UI\n\n Adios !!</i></b></blockquote>"
+SHORT_MSG = "<b>⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ Download ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>"
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜɪ ᴛʜᴇʀᴇ... {first}! 💥\n\nɪ ᴀᴍ ᴀ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ...!\nɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ᴛʜʀᴏᴜɢʜ ᴀ ꜱᴘᴇᴄɪꜰɪᴄ ʟɪɴᴋ....!\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ : <a href=https://t.me/Shirohige_Animes>ᴀɴɪᴍᴇ ᴅʀɪғᴛ</a></b>")
+try:
+    ADMINS=[6179290520]
+    for x in (os.environ.get("ADMINS", "6179290520 6248238332").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}!⚡\n\n🫧ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ʙᴏᴛʜ ᴏꜰ ᴏᴜʀ ᴄʜᴀɴɴᴇʟꜱ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ...!")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+#Short Url or Api
+SHORT_URL = os.environ.get("SHORTNER_URL", "arlinks.in")
+SHORT_API = os.environ.get("SHORTNER_API", "e3e2b415e061aef2edb7fbe280105e48a248a44e")
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - @Hentai_Cinema"
+
+AUTO_DEL = os.environ.get("AUTO_DEL", "True")
+DEL_TIMER = int(os.environ.get("DEL_TIMER", "10800"))
+DEL_MSG = "<b>This File is deleting automatically in {time}. Forward in your Saved Messages..!</b>"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(6179290520)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+#Bhen ke lavdo Credit hataya na ma choddunga wahi aakr salo use karo bas 
